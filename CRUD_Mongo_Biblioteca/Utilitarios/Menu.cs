c8e942a2ccdef9820dc51a0d35a61900496b871e
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-11-19 19:13:22 -0300

    Cria menu principal e inicialização do sistema com splash screen

---
+++ CRUD_Mongo_Biblioteca/Utilitarios/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Mongo_Biblioteca.Utilitarios
+{
+    public class Menu
+    {
+        public void MenuPrincipal()
+        {
+            Console.WriteLine("Digite a opção desejada: (Somente o número da opção)");
+            Console.WriteLine();
+            Console.WriteLine("1 - Relatórios");
+            Console.WriteLine("2 - Inserir documentos");
+            Console.WriteLine("3 - Remover documentos");
+            Console.WriteLine("4 - Atualizar documentos");
+            Console.WriteLine("5 - Sair");
+        }
+        
+    }
+}
