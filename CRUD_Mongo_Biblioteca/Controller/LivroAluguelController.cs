e22afe8b42ba0007e3c0732d886a04ba03f6d991
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-11-25 15:00:25 -0300

    Implementa lógica para atualizações para livros alugados. Carrega backup das entidades do banco de dados.

--- CRUD_Mongo_Biblioteca/Controller/LivroAluguelController.cs
+++ CRUD_Mongo_Biblioteca/Controller/LivroAluguelController.cs
@@ -237,9 +237,92 @@ namespace CRUD_Mongo_Biblioteca.Controller
             return valor;
         }
 
+        //Por lógica, decidimos deixar o usuário alterar apenas o livro. Pois o mesmo pode alterar o leitor através de Aluguel
         public void AlteraLivroAluguel()
         {
+            int opcao = 0;
+            int codigo = 0;
+            bool running = true;
 
+            while (running)
+            {
+                AplicaNulos();
+                Console.WriteLine("Verifique o código do Livro que deseja alterar na lista abaixo: ");
+                Console.WriteLine("Livros alugados...");
+                Console.WriteLine();
+                RelatorioLivrosAlugados();
+                Thread.Sleep(2000);
+                Console.WriteLine("Verifique os livros cadastrados no banco de dados e informe os dados quando solicitados... ");
+                Console.WriteLine("Livros cadastrados... ");
+                RelatorioLivros();
+                Thread.Sleep(2000);
+                Console.WriteLine();
+                Console.WriteLine("Informe o campo que deseja alterar: (informe somente o número da opção)");
+                Console.WriteLine();
+                Console.WriteLine("1 - Alterar livro mantendo a quantidade");
+                Console.WriteLine("2 - Alterar livro alterando a quantidade");                
+                Console.WriteLine("3 - Voltar ao menu de opções anterior");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o código do novo Livro: ");
+                        int novoLivro = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe o código do Aluguel");
+                        int codigoAluguel = int.Parse(Console.ReadLine());
+                        AtualizarLivro(codigo, novoLivro, codigoAluguel);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o código do novo Livro: ");
+                        int novoLivro1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite a quantidade alugada: ");
+                        int novaQuantidade = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe o código do Aluguel");
+                        codigoAluguel = int.Parse(Console.ReadLine());
+                        AtualizarLivroQuantidade(codigo, novoLivro1, novaQuantidade, codigoAluguel);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
+                        break;                    
+                    case 3:
+                        Console.WriteLine("Voltando para o menu de opções...");
+                        Thread.Sleep(2000);
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválidade \nVerifique a opção desejada e tente novamente...");
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        running = false;
+                        break;
+                }
+
+                if(opcao == 3)
+                {
+                    running = false;
+                }
+                else
+                {
+                    Console.Write("Deseja atualizar outro campo? 1 - Sim, 0 - Não (Digite apenas o número para Sim ou Não): ");
+                    opcao = int.Parse(Console.ReadLine());
+                    if (opcao == 0)
+                    {
+                        Console.WriteLine("Retornando ao menu de opções... ");
+                        running = false;
+                    }
+                }
+                
+            }
         }
 
         public async Task<string> PegaTituloLivro(int codigo)
@@ -268,6 +351,76 @@ namespace CRUD_Mongo_Biblioteca.Controller
             itemAluguel.Titulo = null;
             itemAluguel.ValorUnitarioLivro = null;
             itemAluguel.ValorTotalLivro = null;
+        }
+
+        public async void AtualizarLivro(int codigo, int novoLivro,int codigoAluguel)
+        {            
+            var construtor = Builders<LivroAluguel>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo) & construtor.Eq(x => x.CodigoAluguel, codigoAluguel);
+
+            var listaLivros = await conexao.LivroAluguel.Find(condicao).ToListAsync();
+            
+
+            foreach (var doc in listaLivros)
+            {
+                doc.CodigoLivro = novoLivro;
+                doc.Titulo = await PegaTituloLivro(novoLivro);
+                Thread.Sleep(2000);
+                doc.ValorUnitarioLivro = await PegaValorLivro(novoLivro);
+                Thread.Sleep(2000);
+                doc.ValorTotalLivro = doc.QuantidadeLivro * doc.ValorUnitarioLivro;
+                //Método do update no C# com mongo
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"CodigoLivro": novoLivro});
+                //db.Livro.update("CodigoLivro": novoLivro, {"$set": {"Titulo": doc.Titulo});
+                //db.Livro.update("CodigoLivro": novoLivro, {"$set": {"ValorUnitarioLivro": doc.ValorUnitarioLivro});
+                //db.Livro.update("CodigoLivro": novoLivro, {"$set": {"ValorTotalLivro": doc.ValorTotalLivro});
+                await conexao.LivroAluguel.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void AtualizarLivroQuantidade(int codigo, int novoLivro1, int novaQuantidade, int codigoAluguel)
+        {
+            var construtor = Builders<LivroAluguel>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo) & construtor.Eq(x => x.CodigoAluguel, codigoAluguel);
+
+            var listaLivros = await conexao.LivroAluguel.Find(condicao).ToListAsync();
+
+
+            foreach (var doc in listaLivros)
+            {
+                doc.CodigoLivro = novoLivro1;
+                doc.Titulo = await PegaTituloLivro(novoLivro1);
+                Thread.Sleep(2000);
+                doc.ValorUnitarioLivro = await PegaValorLivro(novoLivro1);
+                Thread.Sleep(2000);
+                doc.QuantidadeLivro = novaQuantidade;
+                doc.ValorTotalLivro = novaQuantidade * doc.ValorUnitarioLivro;
+                //Método do update no C# com mongo
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"CodigoLivro": novoLivro});
+                //db.Livro.update("CodigoLivro": novoLivro, {"$set": {"Titulo": doc.Titulo});
+                //db.Livro.update("CodigoLivro": novoLivro, {"$set": {"ValorUnitarioLivro": doc.ValorUnitarioLivro});
+                //db.Livro.update("CodigoLivro": novoLivro, {"$set": {"ValorTotalLivro": doc.ValorTotalLivro});
+                await conexao.LivroAluguel.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void RelatorioLivros()
+        {
+            Console.WriteLine("Listando Documentos");
+
+            var listaLivros = await conexao.Livro.Find(new BsonDocument())
+                                                           .ToListAsync();
+            Console.WriteLine("{0, -5} {1, -32} {2, -4} {3, -4} {4, 9}\n", "Codigo", "Titulo", "Quantidade de Páginas", "Quantidade Disponível", "Valor");
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine("{0, -5} {1, -32} {2, 20} {3, 20} {4, 9}", doc.CodigoLivro, doc.Titulo, doc.Paginas, doc.QuantidadeDisponivel, doc.ValorAluguel);
+            }
+
+            Console.WriteLine("Fim da lista...");
         }
     }
 }
