907011437ec8c0dc1f35c5d0ee6135ebf4d30d6b
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-11-24 21:02:01 -0300

    Ajusta métodos de update para todos os campos da tabela livros

--- CRUD_Mongo_Biblioteca/Controller/LivroController.cs
+++ CRUD_Mongo_Biblioteca/Controller/LivroController.cs
@@ -251,7 +251,7 @@ namespace CRUD_Mongo_Biblioteca.Controller
                     case 1:
                         Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
                         codigo = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Digite o Novo Autor");
+                        Console.WriteLine("Digite o Novo Autor: ");
                         string novoAutor = Console.ReadLine();
                         AtualizarAutor(codigo, novoAutor);
                         Console.WriteLine();
@@ -260,14 +260,59 @@ namespace CRUD_Mongo_Biblioteca.Controller
                         Console.ReadKey();
                         break;
                     case 2:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o Novo Ano: ");
+                        int novoAno = int.Parse(Console.ReadLine());
+                        AtualizarAnoLivro(codigo, novoAno);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
                         break;
                     case 3:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite a nova quantidade de páginas: ");
+                        int novaQuantidade = int.Parse(Console.ReadLine());
+                        AtualizarQPaginasLivro(codigo, novaQuantidade);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
                         break;
                     case 4:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite a nova quantidade disponível: ");
+                        int novaQuantidadeDisp = int.Parse(Console.ReadLine());
+                        AtualizarQDisponivelLivro(codigo, novaQuantidadeDisp);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
                         break;
                     case 5:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o novo valor do Aluguel do livro: ");
+                        int novoValor = int.Parse(Console.ReadLine());
+                        AtualizarValorAluguelLivro(codigo, novoValor);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
                         break;
                     case 6:
+                        Console.WriteLine("Informe o codigo do livro que deseja alterar: ");
+                        codigo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite os novos assuntos do livro, separados por virgula (exemplo: assunto1, assunto2, etc, ... : ");
+                        string novoAssunto = Console.ReadLine();
+                        AtualizarAssuntoLivro(codigo, novoAssunto);
+                        Console.WriteLine();
+                        Console.WriteLine("Registro atualizado com sucesso!");
+                        Console.Write("Pressione qualquer tecla para continuar... ");
+                        Console.ReadKey();
                         break;
                     case 7:
                         Console.WriteLine("Voltando para o menu de opções...");
@@ -337,7 +382,105 @@ namespace CRUD_Mongo_Biblioteca.Controller
                 doc.Autor = novoAutor;
                 //Método do update no C# com mongo
                 //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
                 //db.Livro.update("CodigoLivro": codigo, {"$set": {"Autor": novoAutor});
+                await conexao.Livro.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void AtualizarAnoLivro(int codigo, int novoAno)
+        {
+            var construtor = Builders<Livro>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo);
+
+            var listaLivros = await conexao.Livro.Find(condicao).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                doc.Ano = novoAno;
+                //Método do update no C# com mongo                
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"Ano": novoAno});
+                await conexao.Livro.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void AtualizarQPaginasLivro(int codigo, int novaQuantidade)
+        {
+            var construtor = Builders<Livro>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo);
+
+            var listaLivros = await conexao.Livro.Find(condicao).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                doc.Paginas = novaQuantidade;
+                //Método do update no C# com mongo                
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"Ano": novoAno});
+                await conexao.Livro.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void AtualizarQDisponivelLivro(int codigo, int novaQuantidade)
+        {
+            var construtor = Builders<Livro>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo);
+
+            var listaLivros = await conexao.Livro.Find(condicao).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                doc.QuantidadeDisponivel = novaQuantidade;
+                //Método do update no C# com mongo                
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"Ano": novoAno});
+                await conexao.Livro.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void AtualizarValorAluguelLivro(int codigo, int novoValor)
+        {
+            var construtor = Builders<Livro>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo);
+
+            var listaLivros = await conexao.Livro.Find(condicao).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                doc.ValorAluguel = novoValor;
+                //Método do update no C# com mongo                
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"Ano": novoAno});
+                await conexao.Livro.ReplaceOneAsync(condicao, doc);
+            }
+        }
+
+        public async void AtualizarAssuntoLivro(int codigo, string novosAssuntos)
+        {
+            var construtor = Builders<Livro>.Filter;
+            var condicao = construtor.Eq(x => x.CodigoLivro, codigo);
+
+            var listaLivros = await conexao.Livro.Find(condicao).ToListAsync();
+
+            string[] vetAssunto = novosAssuntos.Split(',');
+            List<string> vetAssunto2 = new List<string>();
+            for (int i = 0; i <= vetAssunto.Length - 1; i++)
+            {
+                vetAssunto2.Add(vetAssunto[i].Trim());
+            }
+
+            foreach (var doc in listaLivros)
+            {
+                doc.Assunto = vetAssunto2;
+                //Método do update no C# com mongo                
+                //Vou inserir o comando aqui demonstrando o tratamento do mesmo.
+
+                //db.Livro.update("CodigoLivro": codigo, {"$set": {"Assunto": ["Assunto"]});
                 await conexao.Livro.ReplaceOneAsync(condicao, doc);
             }
         }
